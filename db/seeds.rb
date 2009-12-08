a9d5094be44521ec6ca099219236982b32ff007d
Russell Jones <spam@codeofficer.com>
2009-12-08 09:14:13 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-user = User.find_by_login("codeofficer")
-user = User.create!({:login => "codeofficer", :email => "spam@codeofficer.com", :password => "netguy", :password_confirmation => "netguy"}) if user.blank?
+%w(codeofficer thatguy).each do |user|
+  User.create!({  :login => user, :email => "#{user}@codeofficer.com", :password => "password", :password_confirmation => "password"}) 
+end
 
-room = Room.find_by_name("Asimov")
-room = Room.create!({:name => "Asimov"}) if room.blank?
+Room.create!({:name => "Asimov"})
